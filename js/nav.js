34e9376c0a64b52c41d9f12973e2f5ef2550d132
samuelsamafolabi@outlook.com <samuelsamafolabi@outlook.com>
2023-07-04 18:16:16 +0100

    nav control js added

---
+++ js/nav.js
@@ -0,0 +1,7 @@
+/**Nav item control */
+let openNav = document.querySelector(".open-menu");
+let nav_pane = document.querySelector(".main-nav-container");
+openNav.addEventListener("click",()=>{
+nav_pane.style.display="block";
+//console.log("click");
+})
