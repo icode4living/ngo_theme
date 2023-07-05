c75a8c4cc5c78ebc52417aa11ac2fe4b3245d11a
SAMUEL AFOLABI <samuelsamafolabi@outlook.com>
2023-07-05 06:37:55 +0100

    responsive design for mobile screens

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,13 @@
 /**Nav item control */
 let openNav = document.querySelector(".open-menu");
-let nav_pane = document.querySelector(".main-nav-container");
+let nav_items = document.querySelector(".main-nav-container");
+let menu = document.querySelector("#main-nav");
 openNav.addEventListener("click",()=>{
-nav_pane.style.display="block";
+    //blur the background
+    document.querySelector(".dark-bg").style.display = "block"
+nav_items.style.display="block";
+menu.classList.remove("main-nav");
+menu.classList.add("m-main-nav")
 //console.log("click");
+document.querySelector("#main-nav");
 })
